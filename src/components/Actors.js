02b853f6b0df1c9eeb20cf095b0b2f7051205ebb
Julie Evans <juliemevans17@gmail.com>
2022-01-24 00:01:14 -0600

    commit tree eb4c1d80aaad05f12facb4071e4f986b86d49446

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,13 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return <>
+    <h1>Actors Page</h1>
+    {actors.map(actor => <div key={actor.name}>
+      <h3>{actor.name}</h3>
+      <ul>{actor.movies.map(movie => <li key={movie}>{movie}</li>)}</ul>
+    </div>)}
+  </>;
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,14 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <>
+    <h1>Movies Page</h1>
+    {movies.map(movie => <div key={movie.title}>
+      <h3>{movie.title}</h3>
+      <p>{movie.time}</p>
+      <ul>{movie.genres.map(genre => <li key={genre}>{genre}</li>)}</ul>
+    </div>)}
+  </>;
 }
 
 export default Movies;
